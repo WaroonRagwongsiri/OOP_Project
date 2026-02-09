4e95de0de37eae6fe0ec9a4d6493a3afbf6ac68a
WachirawichN <wnamgird@gmail.com>
2026-02-09 19:24:20 +0700

    add finish purchase stuff

--- Code/purchaseAPI.py
+++ Code/purchaseAPI.py
@@ -11,21 +11,32 @@ def connection():
     return "SDIYBT"
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 
 =======
 >>>>>>> 38fd179 (purchase stuff)
+=======
+
+
+>>>>>>> 7274d8b (add finish purchase stuff)
 @app.get("/customer_list")
 def customer_list():
     return store.customers
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 7274d8b (add finish purchase stuff)
 @app.post("/create_customer")
 def create_customer(name : str):
     return store.create_customer(name)
 
+<<<<<<< HEAD
 =======
 >>>>>>> 38fd179 (purchase stuff)
+=======
+>>>>>>> 7274d8b (add finish purchase stuff)
 @app.post("/add_product_to_cart")
 def add_product_to_cart(customer_id : str, product_id : str):
     return store.add_product_to_customer(customer_id, product_id)
@@ -37,12 +48,15 @@ def select_product(customer_id : str, product_id : str):
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 @app.post("/create_customer")
 def create_customer(id : str):
     return store.create_customer(id)
 
 >>>>>>> 38fd179 (purchase stuff)
+=======
+>>>>>>> 7274d8b (add finish purchase stuff)
 @app.get("/list_stock")
 def list_stock():
     return store.stock
@@ -52,10 +66,15 @@ def add_product_to_store(product_id : str, product_sn : str, product_price : int
     return store.add_product(product_id, product_sn, product_price)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 
 =======
 >>>>>>> 38fd179 (purchase stuff)
+=======
+
+
+>>>>>>> 7274d8b (add finish purchase stuff)
 @app.post("/purchase")
 def purchase(customer_id : str, payment_method_name : str):
     return store.purchase(customer_id, payment_method_name, [])
